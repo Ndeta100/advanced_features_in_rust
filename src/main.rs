530c06ebda1709f142d0aa7913d6821db168de16
Ndeta Innocent <you@example.com>
2022-08-10 03:04:33 +0300

    advanced traits and using fmt::Display

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use core::slice;
+use core::{fmt, slice};
+use std::{
+    io::repeat,
+    ops::{Add, Deref},
+};
 
 fn main() {
     //Unsafe rust
@@ -43,6 +47,118 @@ fn main() {
     unsafe impl Foo for i32 {
         //Method
     }
+    //      ADVANCED TRAITS
+    pub trait Iterator {
+        type Item;
+        fn next(&mut self) -> Option<Self::Item>;
+    }
+    struct Counter {
+        count: i32,
+    }
+    impl Iterator for Counter {
+        type Item = u32;
+        fn next(&mut self) -> Option<Self::Item> {
+            if self.count < 5 {
+                self.count += 1;
+                Some(self.count as u32)
+            } else {
+                None
+            }
+        }
+    }
+    #[derive(Debug)]
+    struct Point {
+        x: i32,
+        y: i32,
+    }
+    impl Add for Point {
+        type Output = Point;
+        fn add(self, rhs: Self) -> Self::Output {
+            Point {
+                x: self.x + rhs.x,
+                y: self.y + rhs.y,
+            }
+        }
+    }
+    struct Millimeters(u32);
+    struct Meters(u32);
+    impl Add<Meters> for Millimeters {
+        type Output = Millimeters;
+        fn add(self, rhs: Meters) -> Self::Output {
+            Millimeters(self.0 + (rhs.0 * 1000))
+        }
+    }
+    trait Pilot {
+        fn fly(&self);
+    }
+    trait Wizard {
+        fn fly(&self);
+    }
+    struct Human {}
+    impl Pilot for Human {
+        fn fly(&self) {
+            println!("This is your captain speaking");
+        }
+    }
+    impl Wizard for Human {
+        fn fly(&self) {
+            println!("Up!")
+        }
+    }
+    impl Human {
+        fn fly(&self) {
+            println!("*Waving arms furiously");
+        }
+    }
+    trait Animal {
+        fn baby_name() -> String;
+    }
+    struct Dog;
+    impl Dog {
+        fn baby_name() -> String {
+            String::from("Spot")
+        }
+    }
+    impl Animal for Dog {
+        fn baby_name() -> String {
+            String::from("Puppy")
+        }
+    }
+    trait OutlinePoint: fmt::Display {
+        fn outline_point(&self) {
+            let output = self.to_string();
+            let len = output.len();
+            println!("{}", "*".repeat(len + 4));
+            println!("{}", " ".repeat(len + 2));
+            println!("* {} *", output);
+            println!("*{}*", " ".repeat(len + 2));
+            println!("{}", "*".repeat(len + 4));
+        }
+    }
+    impl fmt::Display for Point {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "({} {})", self.x, self.y)
+        }
+    }
+    impl OutlinePoint for Point {}
+    let point_me = Point { x: 3, y: 7 };
+    point_me.outline_point();
+    struct Wrapper(Vec<String>);
+    impl fmt::Display for Wrapper {
+        fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+            write!(f, "[{}]", self.0.join(","))
+        }
+    }
+
+    let w = Wrapper(vec![String::from("Hello"), String::from("world")]);
+    println!("w={}", w);
+    impl Deref for Wrapper {
+        type Target = Vec<String>;
+        fn deref(&self) -> &Self::Target {
+            &self.0
+        }
+    }
+    let w_vec = &*w;
 }
 fn split_at_mut(slice: &mut [i32], mid: usize) -> (&mut [i32], &mut [i32]) {
     let len = slice.len();
